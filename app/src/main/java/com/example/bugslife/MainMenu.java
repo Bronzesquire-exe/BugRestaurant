cc43e4e6e987564519af8d240491a96d96a9afcb
Jaden <jadendixon574@gmail.com>
2025-12-08 08:08:28 -0500

    Order Methods and class, need to add Pick-Up activity and map location

--- app/src/main/java/com/example/bugslife/MainMenu.java
+++ app/src/main/java/com/example/bugslife/MainMenu.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -12,14 +13,30 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import org.w3c.dom.Text;
+
 public class MainMenu extends AppCompatActivity {
 
-
+Order order = new Order();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
-       Button star = findViewById(R.id.stars);
+        Button star = findViewById(R.id.stars);
+
+        TextView chili = findViewById(R.id.chili);
+        chili.setOnClickListener(v -> {
+            order.addItem("Cricket Chili", 15, 1);
+        });
+
+        TextView escamoles = findViewById(R.id.Escamoles);
+        escamoles.setOnClickListener(v ->{
+            order.addItem("Escamoles", 13, 2);
+        });
+        TextView tsukusdani = findViewById(R.id.Tsukusdani);
+        tsukusdani.setOnClickListener(v ->{
+            order.addItem("Tsukusdani", 16, 2);
+        });
 
 star.setOnClickListener(new View.OnClickListener() {
     @Override
---
+++ app/src/main/java/com/example/bugslife/Order.java
@@ -0,0 +1,46 @@
+package com.example.bugslife;
+
+import java.util.ArrayList;
+
+public class Order {
+public static final double tax = 0.02;
+private ArrayList<OrderItem> item = new ArrayList<>();
+
+public void addItem(String name, double price, double extra){
+    item.add(new OrderItem(name,price,extra));
+}
+public ArrayList<OrderItem>getItem(){
+    return item;
+}
+public double subTotal(){
+    double subtotal = 0;
+    for(OrderItem i : item){
+        subtotal += i.getTotalPrice();
+    }
+    return subtotal;
+}
+public double getTax(){
+    return subTotal() * tax;
+}
+public double getTotal(){
+    return subTotal() + getTax();
+}
+
+class OrderItem{
+    private String name;
+    private double basePrice;
+    private double extra;
+
+    public OrderItem(String name, double basePrice, double extra){
+        this.name = name;
+        this.basePrice = basePrice;
+        this.extra = extra;
+    }
+    public String getName(){
+        return name;
+    }
+    public double getTotalPrice(){
+        return basePrice + extra;
+    }
+}
+}
