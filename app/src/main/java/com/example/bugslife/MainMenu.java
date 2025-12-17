065a04703c78510a2029e98deeba494275a91489
jaden <jadendixon574@gmail.com>
2025-12-16 19:07:06 -0500

    Fixed the checkout feature and checkout screen, plan on adding email button later.

--- app/src/main/java/com/example/bugslife/MainMenu.java
+++ app/src/main/java/com/example/bugslife/MainMenu.java
@@ -1,5 +1,7 @@
 package com.example.bugslife;
 
+import static kotlin.reflect.KTypeProjection.star;
+
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
@@ -20,26 +22,57 @@ import java.util.ArrayList;
 
 public class MainMenu extends AppCompatActivity {
 
-    Order order = new Order();
+    private static Order order = new Order();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
         Button star = findViewById(R.id.stars);
+        Button checkout = findViewById(R.id.checkout);
+        checkout.setOnClickListener(v ->
+
+        {
+
+            if (order.getItem().isEmpty()) {
+                Toast.makeText(MainMenu.this, "Please add items to your order first!", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            Intent intent = new Intent(MainMenu.this, Checkout.class);
+
+            intent.putExtra("subtotal", order.subTotal());
+            intent.putExtra("tax", order.getTax());
+            intent.putExtra("Total", order.getTotal());
+
+            ArrayList<String> names = new ArrayList<>();
+
+            for (int i = 0; i < order.getItem().size(); i++) {
+                names.add(order.getItem().get(i).getName());
+            }
+
+            intent.putStringArrayListExtra("items", names);
+
+            startActivity(intent);
+
+
+        });
 
         TextView chili = findViewById(R.id.chili);
         chili.setOnClickListener(v -> {
             order.addItem("Cricket Chili", 15, 1);
+            Toast.makeText(MainMenu.this, "Added Cricket Chili!", Toast.LENGTH_SHORT).show();
         });
 
         TextView escamoles = findViewById(R.id.Escamoles);
         escamoles.setOnClickListener(v -> {
             order.addItem("Escamoles", 13, 2);
+            Toast.makeText(MainMenu.this, "Added Escamoles!", Toast.LENGTH_SHORT).show();
         });
         TextView tsukusdani = findViewById(R.id.Tsukusdani);
         tsukusdani.setOnClickListener(v -> {
             order.addItem("Tsukusdani", 16, 2);
+            Toast.makeText(MainMenu.this, "Added Tsukusdani!", Toast.LENGTH_SHORT).show();
         });
 
         star.setOnClickListener(new View.OnClickListener() {
@@ -49,30 +82,17 @@ public class MainMenu extends AppCompatActivity {
             }
         });
 
-    Button checkout = findViewById(R.id.checkout);
-    checkout.setOnClickListener(v->
 
-    {
-
-
-        Intent intent = new Intent(MainMenu.this, Checkout.class);
-
-        intent.putExtra("subtotal", order.subTotal());
-        intent.putExtra("tax", order.getTax());
-        intent.putExtra("Total", order.getTotal());
-
-        ArrayList<String> names = new ArrayList<>();
-
-        for (int i = 0; i < order.getItem().size(); i++) {
-            names.add(order.getItem().get(i).getName());
-        }
-
-        intent.putStringArrayListExtra("items", names);
-
-        startActivity(intent);
-
-
-    });
+        star.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainMenu.this, "Thanks For 5 Stars", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 }
-}
+
+
+
+
 
